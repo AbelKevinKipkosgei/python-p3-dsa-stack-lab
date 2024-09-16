3d25f14eba084e3ff8b08847f4e58a9c13b5474b
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-09-16 05:10:18 +0300

    Implemented a stack in Python.

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,46 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, items = None, limit = 100):
+        self.items = items if items is not None else []
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) == self.limit:
+            print(f"Stack is already full!!")
+            return
+        self.items.append(item)
+        print(f"{item} pushed to stack.")
 
     def pop(self):
-        pass
+        if self.isEmpty():
+            print(f"Stack is empty no items to pop!!")
+            return None
+        return self.items.pop()
 
     def peek(self):
-        pass
+        if self.isEmpty():
+            print(f"Stack is empty no items to peek!!")
+            return None
+        peek_item = self.items[-1]
+        print(f"{peek_item} is the top on the stack.")
+        return peek_item
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
-
+        return len(self.items) == self.limit
+        
     def search(self, target):
-        pass
+        if target in self.items:
+            index_of_target = self.items.index(target)
+            index_of_top_item = len(self.items) - 1
+            distance = index_of_top_item - index_of_target
+            print(f"Distance is: {distance}")
+            return distance
+        else:
+            print(f"{target} not found in the stack.")
+            return -1
